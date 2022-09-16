8c90e35e93618d2e8302b8a1d41dd74a896d2871
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-16 08:49:40 -0500

    Add and remove tests passing

--- src/TodoList.test.tsx
+++ src/TodoList.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import TodoList from "./TodoList";
 
 jest.mock("uuid", () => ({ v4: () => "123456789" }));
@@ -11,4 +11,24 @@ it("renders", () => {
 it("matches snapshot", () => {
   const { asFragment } = render(<TodoList />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+it("can add a todo", () => {
+  render(<TodoList />);
+  expect(screen.getAllByText("X").length).toBe(1);
+
+  // Fill out form
+  fireEvent.change(screen.getByLabelText("Todo:"), {
+    target: { value: "Test todo" },
+  });
+  fireEvent.click(screen.getByText("New Todo"));
+  expect(screen.getAllByText("X").length).toBe(2);
+});
+
+it("can remove a todo", () => {
+  render(<TodoList />);
+  const deleteButton = screen.getByText("X");
+  expect(deleteButton).toBeInTheDocument();
+  fireEvent.click(deleteButton);
+  expect(deleteButton).not.toBeInTheDocument();
 });
