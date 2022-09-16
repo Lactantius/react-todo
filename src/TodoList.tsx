4d55438995bd475a493317b21f407ce0c9dbbe49
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-15 23:50:10 -0500

    Fill out TodoList

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+
+import NewTodoForm from "./NewTodoForm";
+import Todo from "./Todo";
 import "./TodoList.css";
 
 function TodoList(): JSX.Element {
-  return <div className="TodoList"></div>;
+  const [todos, setTodos] = useState([{ text: "Make some todos", id: uuid() }]);
+
+  const addTodo = (todo: string) =>
+    setTodos((oldTodos) => [...oldTodos, { text: todo, id: uuid() }]);
+
+  const removeTodo = (id: string) =>
+    setTodos((oldTodos) => oldTodos.filter((todo) => id !== todo.id));
+
+  return (
+    <div className="TodoList">
+      <NewTodoForm add={addTodo} />
+      <div className="TodoList-container">
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            remove={removeTodo}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default TodoList;
